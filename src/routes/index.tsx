61517bebd928889ba0d5bef06cd954db87cd6d33
harshchhabra-closeloop <harsh.chhabra@closeloop.com>
2023-06-02 10:32:55 +0530

    fixed typescript issues

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
 import { CSRBoard, Dashboard, Login, QueryBuilderComponent } from '../pages';
 
@@ -12,33 +17,21 @@ const queryClient = new QueryClient({
   },
 });
 
-const privateRoutes = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard />,
-  },
-  {
-    path: '/dashboard/boards/:board',
-    element: <CSRBoard />,
-  },
-  {
-    path: '/query',
-    element: <QueryBuilderComponent />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="" element={<Login />} />
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="dashboard/boards/:board" element={<CSRBoard />} />
+      <Route path="query" element={<QueryBuilderComponent />} />
+    </Route>,
+  ),
+);
 
 function AppRoutes() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={privateRoutes} />
+      <RouterProvider router={router} />
       <ReactQueryDevtools position="bottom-right" />
     </QueryClientProvider>
   );
