3f8cf78374bff84189b0fa46397aefabb9a78cba
harshchhabra-closeloop <harsh.chhabra@closeloop.com>
2023-06-02 10:23:42 +0530

    added new page for query

--- src/pages/query-builder/index.tsx
+++ src/pages/query-builder/index.tsx
@@ -1,7 +1,14 @@
 import { QueryBuilder } from 'react-querybuilder';
 
 function QueryBuilderComponent() {
-  return <QueryBuilder />;
+  return (
+    <div className="p-4">
+      <h1 className="mb-3 font-montserrat font-extrabold text-3xl">
+        React Query Builder
+      </h1>
+      <QueryBuilder />
+    </div>
+  );
 }
 
 export default QueryBuilderComponent;
